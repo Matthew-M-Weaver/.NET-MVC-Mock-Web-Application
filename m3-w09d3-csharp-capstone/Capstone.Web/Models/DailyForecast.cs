04e7e0206ec315e349efa479a97efcff20bb5c57
Matt Weaver <mclaughlin.weaver@gmail.com>
2018-03-14 11:26:18 -0400

    forecast model

---
+++ m3-w09d3-csharp-capstone/Capstone.Web/Models/DailyForecast.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class DailyForecast
+    {
+        public int Day { get; set; }
+        public int Low { get; set; }
+        public int High { get; set; }
+        public string Conditions { get; set; }
+
+        public void ConvertToCelsius()
+        {
+            Low = (Low - 32) * (5 / 9);
+            High = (High - 32) * (5 / 9);
+        }
+
+        public ConvertToFarenheit()
+        {
+            Low = Low * (9 / 5) + 32;
+            High = High * (9 / 5) + 32;
+        }
+    }
+}
